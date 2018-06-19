13481cd0d542654694deaaea576e6c4bf6d7d669
comeetie <etienne.come@gmail.com>
2018-06-19 08:46:23 +0200

    clean ex

--- pypixgrid/exemples/insee/metropole/make.sql
+++
@@ -1,4 +0,0 @@
-create table carpop as select cg.id, (regexp_replace(regexp_replace(cg.id, 'LAEA200M_N', ''),'E.*',''))::integer as i, (regexp_replace(regexp_replace(cg.id, 'LAEA200M_N', ''),'.*E',''))::integer as j,
-	wkb_geometry, ind_c as pop from car_m as cg, carreaux as c where c.id=cg.id;
-
-
